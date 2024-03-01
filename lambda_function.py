af676fcc6b5f89ac2e9eacae68fbe27d1e878210
Tomas Storc <tomas.storc@gmail.com>
2024-03-01 15:11:29 +0100

    feat: add time

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,20 @@
 import json
 import oracledb
+import time
 
 def lambda_handler(event, context):
-    connection = oracledb.connect(user='scott', password='mypw', dsn='localhost/oraclepdb1')
-    with connection.cursor() as cursor:
-        for row in cursor.execute('select city from locations'):
-            print(row)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!!')
-    }
+    start = time.time()
+    try:
+        connection = oracledb.connect(user='scott', password='mypw', dsn='localhost/oraclepdb1')
+        with connection.cursor() as cursor:
+            for row in cursor.execute('select city from locations'):
+                print(row)
+        return {
+            'statusCode': 200,
+            'body': json.dumps({"status": "success", "time": f"{time.time() - start}"})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({"status": "failed", "time": f"{time.time() - start}"})
+        }
