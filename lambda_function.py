bd413f46c1495f638b764919e091510b788ebfd8
Tomas Storc <tomas.storc@gmail.com>
2024-03-01 14:28:43 +0100

    feat: init commit

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+import json
+import oracledb
+
+def lambda_handler(event, context):
+    connection = oracledb.connect(user='scott', password='mypw', dsn='localhost/oraclepdb1')
+    with connection.cursor() as cursor:
+        for row in cursor.execute('select city from locations'):
+            print(row)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
